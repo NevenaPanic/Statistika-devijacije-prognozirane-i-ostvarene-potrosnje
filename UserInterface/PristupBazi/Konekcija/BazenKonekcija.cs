b8c6404f2a07c857d840c517734b05dfa5ae3e14
NevenaPanic <nevena.panic99@gmail.com>
2021-06-01 17:46:53 +0200

    Nedelja 3 izrade projekta

---
+++ UserInterface/PristupBazi/Konekcija/BazenKonekcija.cs
@@ -0,0 +1,42 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PristupBazi.Konekcija
+{
+    public class BazenKonekcija : IDisposable
+    {
+        private static IDbConnection instance = null;
+
+        public static IDbConnection GetConnection()
+        {
+            if (instance == null || instance.State == System.Data.ConnectionState.Closed)
+            {
+                OracleConnectionStringBuilder ocsb = new OracleConnectionStringBuilder();
+                ocsb.DataSource = Konekcija.ParametriKonekcije.DATA_SOURCE;
+                ocsb.UserID = Konekcija.ParametriKonekcije.USER_ID;
+                ocsb.Password = Konekcija.ParametriKonekcije.PASSWORD;
+                ocsb.Pooling = true;
+                ocsb.MinPoolSize = 1;
+                ocsb.MaxPoolSize = 10;
+                ocsb.IncrPoolSize = 3;
+                instance = new OracleConnection(ocsb.ConnectionString);
+
+            }
+            return instance;
+        }
+
+        public void Dispose()
+        {
+            if (instance != null)
+            {
+                instance.Close();
+                instance.Dispose();
+            }
+        }
+    }
+}
